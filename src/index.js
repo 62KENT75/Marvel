d3e736746dcc36ef80585149ee56b1de0a26e188
Sergey_Gershun <gershun62@gmail.com>
2024-03-08 13:46:14 +0200

    random character

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
+import reportWebVitals from './reportWebVitals';
 import './style/style.scss';
 
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
